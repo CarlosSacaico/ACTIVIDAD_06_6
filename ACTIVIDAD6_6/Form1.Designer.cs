98e0d69bf3b6bcb3c159d08aa3b5e93bda6298a9
Carlos Sacaico <m06473c@upla.edu.pe>
2021-07-31 18:04:20 -0500

    Agregar archivos de proyecto.

---
+++ ACTIVIDAD6_6/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace ACTIVIDAD6_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.btnestadisticas = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.lvLibro = new System.Windows.Forms.ListView();
+            this.lvEstadistica = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnregistrar
+            // 
+            this.btnregistrar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnregistrar.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregistrar.ForeColor = System.Drawing.Color.GreenYellow;
+            this.btnregistrar.Location = new System.Drawing.Point(40, 382);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(119, 50);
+            this.btnregistrar.TabIndex = 0;
+            this.btnregistrar.Text = "REGISTRAR";
+            this.btnregistrar.UseVisualStyleBackColor = false;
+            this.btnregistrar.Click += new System.EventHandler(this.btnregistrar_Click);
+            // 
+            // btnestadisticas
+            // 
+            this.btnestadisticas.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnestadisticas.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnestadisticas.ForeColor = System.Drawing.Color.GreenYellow;
+            this.btnestadisticas.Location = new System.Drawing.Point(40, 471);
+            this.btnestadisticas.Name = "btnestadisticas";
+            this.btnestadisticas.Size = new System.Drawing.Size(145, 52);
+            this.btnestadisticas.TabIndex = 1;
+            this.btnestadisticas.Text = "ESTADISTICAS";
+            this.btnestadisticas.UseVisualStyleBackColor = false;
+            this.btnestadisticas.Click += new System.EventHandler(this.btnestadisticas_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NUMERO";
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero.Location = new System.Drawing.Point(100, 80);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(130, 39);
+            this.lblnumero.TabIndex = 3;
+            this.lblnumero.Text = "lblnumero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(36, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "TITULO";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(41, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 19);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "CATEGORIA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(41, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 19);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "COSTO $";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(40, 169);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(167, 20);
+            this.txtTitulo.TabIndex = 7;
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(42, 318);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(165, 20);
+            this.txtCosto.TabIndex = 8;
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Items.AddRange(new object[] {
+            "Gestion",
+            "Ingenieria",
+            "Programacion",
+            "Base de Datos"});
+            this.cboCategoria.Location = new System.Drawing.Point(40, 248);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(165, 21);
+            this.cboCategoria.TabIndex = 9;
+            this.cboCategoria.Text = "Seleccionar";
+            // 
+            // lvLibro
+            // 
+            this.lvLibro.BackColor = System.Drawing.Color.Linen;
+            this.lvLibro.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvLibro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvLibro.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvLibro.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lvLibro.GridLines = true;
+            this.lvLibro.HideSelection = false;
+            this.lvLibro.Location = new System.Drawing.Point(239, 80);
+            this.lvLibro.Name = "lvLibro";
+            this.lvLibro.Size = new System.Drawing.Size(776, 175);
+            this.lvLibro.TabIndex = 10;
+            this.lvLibro.UseCompatibleStateImageBehavior = false;
+            this.lvLibro.View = System.Windows.Forms.View.Details;
+            // 
+            // lvEstadistica
+            // 
+            this.lvEstadistica.BackColor = System.Drawing.Color.Linen;
+            this.lvEstadistica.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvEstadistica.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8});
+            this.lvEstadistica.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvEstadistica.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lvEstadistica.GridLines = true;
+            this.lvEstadistica.HideSelection = false;
+            this.lvEstadistica.Location = new System.Drawing.Point(239, 318);
+            this.lvEstadistica.Name = "lvEstadistica";
+            this.lvEstadistica.Size = new System.Drawing.Size(750, 157);
+            this.lvEstadistica.TabIndex = 11;
+            this.lvEstadistica.UseCompatibleStateImageBehavior = false;
+            this.lvEstadistica.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "NUMERO";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "TITULO";
+            this.columnHeader2.Width = 236;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "CATEGORIA";
+            this.columnHeader3.Width = 106;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "PRECIO REAL";
+            this.columnHeader4.Width = 94;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "DESCUENTO";
+            this.columnHeader5.Width = 120;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "PRECIO DE VENTA";
+            this.columnHeader6.Width = 116;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "CONDICION";
+            this.columnHeader7.Width = 419;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "VALOR";
+            this.columnHeader8.Width = 313;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(86, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(823, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "CONTROL DE REGISTRO DE LIBROS DE LA  EDITORA BOOKS-NOW";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(1049, 575);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lvEstadistica);
+            this.Controls.Add(this.lvLibro);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblnumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnestadisticas);
+            this.Controls.Add(this.btnregistrar);
+            this.Name = "Form1";
+            this.Text = "BOOKS-NOW";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Button btnestadisticas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.ListView lvLibro;
+        private System.Windows.Forms.ListView lvEstadistica;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ACTIVIDAD6_6/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACTIVIDAD6_6
+{
+    public partial class Form1 : Form
+    {
+        static int contador;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblnumero.Text = generarnumero();
+        }
+        //Implementacion de expresion lambda
+        Func<string> generarnumero = () =>
+        {
+            contador++;
+            return contador.ToString("0000");
+        };
+        //Metodo que capturan los valores
+        int getNumero()
+        {
+            return int.Parse(lblnumero.Text);
+        }
+        string getTitulo()
+        {
+            return txtTitulo.Text;
+        }
+        double getCosto()
+        {
+            return double.Parse(txtCosto.Text);
+        }
+        string getCategoria()
+        {
+            return cboCategoria.Text;
+        }
+        Func<string, double, double> asignaDescuento = (categoria, costo) =>
+        {
+            double descuento = 0;
+            switch (categoria)
+            {
+                case "Gestion": descuento = 10.0 / 100 * costo; break;
+                case "Ingenieria": descuento = 12.0 / 100 * costo; break;
+                case "Programacion": descuento = 20.0 / 100 * costo; break;
+                case "Base de datos": descuento = 15.0 / 100 * costo; break;
+            }
+            return descuento;
+        };
+        Func<double, double, double> calculaPrecioVenta = (costo, descuento) => costo - descuento;
+        //Calculando las estadisticas
+        //Monto total de descuentos
+        double calculaTotalDescuentos()
+        {
+            double total = 0;
+            for(int i=0; i < lvLibro.Items.Count; i++)
+            {
+                total += double.Parse(lvLibro.Items[i].SubItems[4].Text);
+            }
+            return total;
+        }
+        //Libro con el precio mas alto
+        string libroMasAlto()
+        {
+            double mayor = double.Parse(lvLibro.Items[0].SubItems[5].Text);
+            int posicion = 0;
+            for(int i = 0; i < lvLibro.Items.Count; i++)
+            {
+                if(double.Parse(lvLibro.Items[i].SubItems[5].Text)> mayor)
+                {
+                    posicion = i;
+                }
+            }
+            return lvLibro.Items[posicion].SubItems[1].Text;
+        }
+        //Imprimir el registro de ventas
+        void imprimirRegistro(double descuento,double precioVenta)
+        {
+            ListViewItem fila = new ListViewItem(getNumero().ToString());
+            fila.SubItems.Add(getTitulo());
+            fila.SubItems.Add(getCategoria());
+            fila.SubItems.Add(getCosto().ToString("0.00"));
+            fila.SubItems.Add(descuento.ToString("0.00"));
+            fila.SubItems.Add(precioVenta.ToString("0.00"));
+            lvLibro.Items.Add(fila);
+        }
+        //Imprimir estadisticas
+        void imprimirEstadisticas(double totalDescuentos,string LibroAlto)
+        {
+            lvEstadistica.Items.Clear();
+            string[] elementosFila = new string[2];
+            ListViewItem row;
+
+            elementosFila[0] = "Monto total acumulado de descuentos";
+            elementosFila[1] = totalDescuentos.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadistica.Items.Add(row);
+
+            elementosFila[0] = "El libro con el precio de venta mas caro";
+            elementosFila[1] = LibroAlto;
+            row = new ListViewItem(elementosFila);
+            lvEstadistica.Items.Add(row);
+
+        }
+        //Metodo para la validacion de datos
+        string valida()
+        {
+            if (txtTitulo.Text.Trim().Length == 0)
+            {
+                txtTitulo.Focus();
+                return "Titulo del libro";
+            }else if (cboCategoria.SelectedIndex == -1)
+            {
+                cboCategoria.Focus();
+                return "Categoria del libro";
+            }else if (txtCosto.Text.Trim().Length == 0)
+            {
+                txtCosto.Focus();
+                return "Costo del libro";
+            }
+            return "";
+        }
+
+        private void btnregistrar_Click(object sender, EventArgs e)
+        {
+            if (valida() == "")
+            {
+                //Capturando los datos de los formularios
+                double costo = getCosto();
+                string categoria = getCategoria();
+                double descuento = asignaDescuento(categoria, costo);
+                double precioVenta = calculaPrecioVenta(costo, descuento);
+                //Enviando a la impresion 
+                imprimirRegistro(descuento, precioVenta);
+
+                lblnumero.Text = generarnumero();
+            }
+            else
+                MessageBox.Show("El error se encuentra en " + valida());
+        }
+
+        private void btnestadisticas_Click(object sender, EventArgs e)
+        {
+            double totalDescuentos = calculaTotalDescuentos();
+            string libroalto = libroMasAlto();
+
+            imprimirEstadisticas(totalDescuentos, libroalto);
+        }
+    }
+}
